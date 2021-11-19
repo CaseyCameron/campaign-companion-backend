c5f4aa817984405499f2b044c5a0e0f3ea09b770
CaseyCameron <mcasey.cameron@gmail.com>
2021-11-19 09:58:41 -0800

    Passing Campaings Integration tests

--- __tests__/campaign.test.js
+++ __tests__/campaign.test.js
@@ -9,7 +9,14 @@ const user = {
   email: 'testMaster@gamemaster.com'
 };
 
-const campaign = {
+const campaign1 = {
+  name: 'First Test Campaign',
+  description: 'First Test description',
+  image: 'https://cdn.discordapp.com/attachments/716731135501271101/871626952958672956/2Q.png',
+  gameMaster: user.authId,
+};
+
+const campaign2 = {
   name: 'Second Test Campaign',
   description: 'Second Test description',
   image: 'https://cdn.discordapp.com/attachments/716731135501271101/871626952958672956/2Q.png',
@@ -36,7 +43,7 @@ describe('demo routes', () => {
   it('POSTs a campaign', async () => {
     const res = await request(app)
       .post('/api/v1/campaigns')
-      .send(campaign);
+      .send(campaign1);
 
     expect(res.body).toEqual({ id: 1, ...res.body });
   });
@@ -45,6 +52,41 @@ describe('demo routes', () => {
     const res = await request(app)
       .get('/api/v1/campaigns/1');
 
-    expect(res.body).toEqual({ id: 1, ...campaign });
+    expect(res.body).toEqual({ id: 1, ...campaign1 });
+  });
+
+  it('POSTS and gets all campaigns', async () => {
+    await request(app)
+      .post('/api/v1/campaigns')
+      .send(campaign2);
+
+    const res = await request(app)
+      .get('/api/v1/campaigns/');
+
+    expect(res.body).toEqual([{ id: 1, ...campaign1 }, { id:2, ...campaign2 }]);
+  });
+
+  it('Updates via PUT the second campaign', async () => {    
+    await request(app)
+      .put('/api/v1/campaigns/2')
+      .send({ description: 'updated' });
+
+    const res = await request(app)
+      .get('/api/v1/campaigns/2');
+
+    expect(res.body).toEqual({
+      id: 2,
+      name: 'Second Test Campaign',
+      description: 'updated',
+      image: 'https://cdn.discordapp.com/attachments/716731135501271101/871626952958672956/2Q.png',
+      gameMaster: 'googleauth|1234',
+    });
+  });
+
+  it('DELETEs the first campaign', async () => {
+    const res = await request(app)
+      .delete('/api/v1/campaigns/1');
+
+    expect(res.body).toEqual({ delete: 'complete' });
   });
 });
